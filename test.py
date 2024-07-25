802fd95ae00fe7caa87fb9a02c4ca355df96ccc6
musakhawaja <56578327+musakhawaja@users.noreply.github.com>
2024-07-25 18:26:25 +0500

    Update test.py

--- test.py
+++ test.py
@@ -31,22 +31,5 @@ for _, row in df.iterrows():
 
 pattern = r'(\w+): ([^\n]+)'
 child_splitter = RegexTextSplitter(pattern=pattern)
-
-embeddings = OpenAIEmbeddings(model="text-embedding-ada-002", chunk_size=500)
-vector_store = PineconeVectorStore(embedding=embeddings, index_name=index_name)
-
-# bucket_name = "test"
-# store = GCSFileStore(bucket_name)
-store = MongoDBFileStore(connection_string=uri, database_name="DB_NAME", collection_name="COL_NAME")
-
-retriever = ParentDocumentRetriever(
-    vectorstore=vector_store,
-    docstore=store,
-    child_splitter=child_splitter
-)
-
-retriever.add_documents(documents, ids=id)
-retrieved_docs = retriever.invoke("wisconsin")
-
-print(len(retrieved_docs))
-print(retrieved_docs[0])
+result = child_splitter.split_documents(documents)
+print(result)
