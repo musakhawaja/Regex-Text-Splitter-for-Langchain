be03bdf0bb4f2d12d88b2325c8ded5d2a92864af
musakhawaja <56578327+musakhawaja@users.noreply.github.com>
2024-07-25 18:27:20 +0500

    Update test.py

--- test.py
+++ test.py
@@ -1,22 +1,6 @@
-from langchain.retrievers import ParentDocumentRetriever
 import pandas as pd
 from langchain.schema import Document
-import os
-from langchain_pinecone import PineconeVectorStore
-from langchain_openai import OpenAIEmbeddings
-from dotenv import load_dotenv
-from GCStore import GCSFileStore
-from MongoDBStore import MongoDBFileStore
 from RegexSplitter import RegexTextSplitter
-load_dotenv()
-
-uri = os.getenv("MONGO_URI")
-
-path = os.path.join(os.getcwd(), "YOUR_KEY.json")
-os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = path
-
-index_name = "test-db"
-
 
 df = pd.read_csv("test.csv", encoding='utf-8', low_memory=False)
 documents = []
